ea36d58df318fc96cd532c286617c2319e498f50
Sandr0x00 <sandro.bauer.93@gmail.com>
2019-07-27 23:35:35 +0200

    Added some Security headers

--- server.go
+++ server.go
@@ -7,6 +7,7 @@ import (
 	"os"
 
 	"github.com/gorilla/mux"
+	"github.com/unrolled/secure"
 )
 
 func logRequest(handler http.Handler) http.Handler {
@@ -19,7 +20,30 @@ func logRequest(handler http.Handler) http.Handler {
 func main() {
 	port := 8081
 
+	secureMiddleware := secure.New(secure.Options{
+		AllowedHosts:         []string{"sandr0\\.tk"},
+		AllowedHostsAreRegex: false,
+		// HostsProxyHeaders:    []string{"X-Forwarded-Host"},
+		SSLRedirect: false,
+		// SSLHost:               "ssl.example.com",
+		// SSLProxyHeaders:       map[string]string{"X-Forwarded-Proto": "https"},
+		STSSeconds:            31536000,
+		STSIncludeSubdomains:  true,
+		STSPreload:            true,
+		FrameDeny:             true,
+		ContentTypeNosniff:    true,
+		BrowserXssFilter:      true,
+		ContentSecurityPolicy: "default-src 'self'",
+		// PublicKey:             `pin-sha256="base64+primary=="; pin-sha256="base64+backup=="; max-age=5184000; includeSubdomains; report-uri="https://www.example.com/hpkp-report"`,
+		ReferrerPolicy: "same-origin",
+		FeaturePolicy:  "vibrate 'none'; geolocation 'none'; speaker 'none'; camera 'none'; micophone 'none'; notifications 'none';",
+		IsDevelopment:  false,
+	})
+
 	r := mux.NewRouter()
+
+	r.Use(logRequest)
+	r.Use(secureMiddleware.Handler)
 
 	r.PathPrefix("/shared/").Handler(http.StripPrefix("/shared/", http.FileServer(http.Dir("shared"))))
 	r.PathPrefix("/").Handler(http.FileServer(http.Dir("static")))
@@ -27,7 +51,7 @@ func main() {
 
 	hostname, _ := os.Hostname()
 	fmt.Printf("Starting server on http://%s:%d\n", hostname, port)
-	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), logRequest(r)); err != nil {
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil); err != nil {
 		log.Fatal(err)
 	}
 }
