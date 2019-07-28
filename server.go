7fe79ef123ff65082e3628a1afd008d95304a983
Sandr0x00 <sandro.bauer.93@gmail.com>
2019-07-29 00:26:19 +0200

    logger

--- server.go
+++ server.go
@@ -18,7 +18,7 @@ const (
 
 func logRequest(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
+		log.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
 		handler.ServeHTTP(w, r)
 	})
 }
