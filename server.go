603b05de656ad90edfce39c344fe1675cd582cb8
Sandr0x00 <sandro.bauer.93@gmail.com>
2019-09-30 13:03:06 +0200

    Add recipes.

--- server.go
+++ server.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/http/httputil"
+	"net/url"
 	"os"
 
 	"github.com/gorilla/mux"
@@ -23,6 +25,26 @@ func logRequest(handler http.Handler) http.Handler {
 	})
 }
 
+// Serve a reverse proxy for a given url
+var serveReverseProxy = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	// parse the url
+	url, _ := url.Parse("http://localhost:8082")
+
+	log.Printf("%s %s %s\n", url, r.Method, r.URL)
+
+	// create the reverse proxy
+	proxy := httputil.NewSingleHostReverseProxy(url)
+
+	// Update the headers to allow for SSL redirection
+	r.URL.Host = url.Host
+	r.URL.Scheme = url.Scheme
+	r.Header.Set("X-Forwarded-Host", r.Header.Get("Host"))
+	r.Host = url.Host
+
+	// Note that ServeHttp is non blocking and uses a go routine under the hood
+	proxy.ServeHTTP(w, r)
+})
+
 func main() {
 	secureMiddleware := secure.New(secure.Options{
 		// AllowedHosts:         []string{"sandr0\\.tk"},
@@ -31,14 +53,21 @@ func main() {
 		SSLRedirect: false,
 		SSLHost:     "sandro.tk",
 		// SSLProxyHeaders:       map[string]string{"X-Forwarded-Proto": "https"},
-		STSSeconds:            31536000,
-		STSIncludeSubdomains:  true,
-		STSPreload:            true,
-		ForceSTSHeader:        true,
-		FrameDeny:             true,
-		ContentTypeNosniff:    true,
-		BrowserXssFilter:      true,
-		ContentSecurityPolicy: "default-src 'self' use.fontawesome.com; font-src 'self' use.fontawesome.com; img-src 'self'; style-src 'unsafe-inline' use.fontawesome.com stackpath.bootstrapcdn.com; script-src 'self' stackpath.bootstrapcdn.com; base-uri 'none'; form-action 'self';",
+		STSSeconds:           31536000,
+		STSIncludeSubdomains: true,
+		STSPreload:           true,
+		ForceSTSHeader:       true,
+		FrameDeny:            true,
+		ContentTypeNosniff:   true,
+		BrowserXssFilter:     true,
+		ContentSecurityPolicy: `
+			default-src 'self' use.fontawesome.com;
+			font-src 'self' use.fontawesome.com;
+			img-src 'self';
+			style-src 'self' 'unsafe-inline' use.fontawesome.com stackpath.bootstrapcdn.com;
+			script-src 'self' 'unsafe-inline' unpkg.com stackpath.bootstrapcdn.com;
+			base-uri 'none'; form-action 'self';
+		`,
 		// PublicKey:             `pin-sha256="base64+primary=="; pin-sha256="base64+backup=="; max-age=5184000; includeSubdomains; report-uri="https://www.example.com/hpkp-report"`,
 		ReferrerPolicy: "same-origin",
 		FeaturePolicy:  "vibrate 'none'; geolocation 'none'; speaker 'none'; camera 'none'; microphone 'none'; notifications 'none';",
@@ -51,6 +80,7 @@ func main() {
 
 	// no securemiddleware in shared
 	r.PathPrefix("/shared/").Handler(http.StripPrefix("/shared/", http.FileServer(http.Dir("shared"))))
+	r.PathPrefix("/recipes/").Handler(secureMiddleware.Handler(serveReverseProxy))
 	r.PathPrefix("/").Handler(secureMiddleware.Handler(http.FileServer(http.Dir("static"))))
 	http.Handle("/", r)
 
