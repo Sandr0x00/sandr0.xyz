6a1b8dded1b9632ea1314850360eaf4f8a137ef9
sandr0 <s@sandr0.xyz>
2023-05-18 15:33:38 +0200

    added report.py

---
+++ report.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import os
+import subprocess
+import re
+
+done = []
+
+for f in os.listdir("secured"):
+    if (m := re.match(r"^report.(\d{4}-\d\d).html$", f)):
+        done.append(m.group(1))
+
+for f in os.listdir("logs"):
+    if (m := re.match(r"^access.(\d{4}-\d\d).log$", f)):
+        if m.group(1) not in done:
+            subprocess.run(["goaccess", f"logs/access.{m.group(1)}.log", "-a", "-o", f"secured/report.{m.group(1)}.html"])
+
+subprocess.run(["goaccess", "access.log", "-a", "-o", "secured/report.html"])
